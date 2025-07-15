aaa04601ed48cfa3030a476bd921d9b1ccae2354
The1stBurb <eli.cooper@ucas-edu.net>
2025-07-15 13:50:35 -0600

    buidling logic gates, wires

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 import pygame
 from P5game import *
+from random import choice
 pygame.display.set_caption("Game About Sentient Rocks!")
-logicBtns={"and":BUTTON(0,300,50,30,(100,0,0),"AND",(0,0,0),textX=5,textY=5)}
+allIDs=[]
+charID="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+def getID(chars=5):
+    newID=""
+    for i in chars:
+        newID+=choice(charID)
+    if newID in allIDs:
+        return getID(chars)
+    return newID
+logicBtns={"nand":BUTTON(0,300,50,30,(100,0,0),"NAND",(0,0,0),textX=5,textY=5)}
+logicIOsz=20
+class logicGateCondensed:
+    def __init__(self,name,inputs,outputs,logic):
+        self.name=name
+        self.inps=inputs
+        self.inpVal=[-1 for i in range(len(self.inps))]
+        self.outs=outputs
+        self.outVal=[-1 for i in range(len(self.inps))]
+        self.logic=logic
+    def render(self,x,y):
+        ht=max(len(self.inps,self.outs))
+        fill(200)
+        rect(x,y,100,ht*logicIOsz)
+        for inp_y,i in enumerate(self.inpVal):
+            if i==1:fill(255,0,0)
+            elif i==0:fill(150,0,0)
+            else:fill(0)
+            ellipse(x,y+inp_y*logicIOsz,logicIOsz*1.5)
+        for out_y,i in enumerate(self.outVal):
+            if i==1:fill(255,0,0)
+            elif i==0:fill(150,0,0)
+            else:fill(0)
+            ellipse(x+100,y+out_y*logicIOsz,logicIOsz*1.5)
+class Wire:
+    def __init__(self,inputId,outputId):
+
+class LogicGate:
+    def __init__(self,name,inputs,outputs,wires,gates):
+        self.name=name
+        self.inps=inputs
+        self.inpVal=[-1 for i in range(len(self.inps))]
+        self.outs=outputs
+        self.outVal=[-1 for i in range(len(self.inps))]
+        self.wires=wires
+        self.gates=gates
+logicGates={"nand":logicGateCondensed("NAND",["a","b"],["out"],"not(a and b)")}
 running = True
 while running:
     mouse.update()
